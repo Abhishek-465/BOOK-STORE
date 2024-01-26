d4efa7c23cdb3e78eb00171d54ea12a5a7785fc9
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-01-26 09:40:48 +0530

    Update OtherBooks.jsx

--- mern-client/src/home/OtherBooks.jsx
+++ mern-client/src/home/OtherBooks.jsx
@@ -5,7 +5,7 @@ const OtherBooks = () => {
     const[books,setBooks]=useState([]);
 
     useEffect(()=>{
-        fetch("https://book-store-api-blond.vercel.app/all-books").then(res=>res.json()).then(data => setBooks(data.slice(4,9)))
+        fetch("https://localhost:5000/all-books").then(res=>res.json()).then(data => setBooks(data.slice(4,9)))
     },[])
   return (
     <div><BookCards books ={books} headline="Other Books"/></div>
