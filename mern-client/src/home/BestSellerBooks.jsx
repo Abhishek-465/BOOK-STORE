34d8ea4cd8818e3836dfadf5b836638e5e031890
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-01-26 09:41:18 +0530

    Update BestSellerBooks.jsx

--- mern-client/src/home/BestSellerBooks.jsx
+++ mern-client/src/home/BestSellerBooks.jsx
@@ -5,7 +5,7 @@ const BestSellerBooks = () => {
     const[books,setBooks]=useState([]);
 
     useEffect(()=>{
-        fetch("https://book-store-api-blond.vercel.app/all-books").then(res=>res.json()).then(data => setBooks(data.slice(0,6)))
+        fetch("https://localhost:5000/all-books").then(res=>res.json()).then(data => setBooks(data.slice(0,6)))
     },[])
   return (
     <div><BookCards books ={books} headline="Best Seller Books"/></div>
