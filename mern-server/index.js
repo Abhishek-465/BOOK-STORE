e3e2424d7f608d005dc958d73ae7bd6aab5504d5
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-01-26 09:10:36 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors=require('cors');
 //middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials: true
+  }
+));
 app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
