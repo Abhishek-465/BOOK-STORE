0010187fc94d8d13c3dc87d661f7ed8c33c4602c
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-03-23 19:53:23 +0530

    Update BestSellerBooks.jsx

--- mern-client/src/home/BestSellerBooks.jsx
+++ mern-client/src/home/BestSellerBooks.jsx
@@ -5,7 +5,7 @@ const BestSellerBooks = () => {
     const[books,setBooks]=useState([]);
 
     useEffect(()=>{
-        fetch("https://localhost:5000/all-books").then(res=>res.json()).then(data => setBooks(data.slice(0,6)))
+        fetch("https://book-store-mqzx.onrender.com/all-books").then(res=>res.json()).then(data => setBooks(data.slice(0,6)))
     },[])
   return (
     <div><BookCards books ={books} headline="Best Seller Books"/></div>
