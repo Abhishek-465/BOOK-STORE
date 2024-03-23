4f3cc054cdcb2841ec88babd35aaddebd5349cd2
Abhishek Bhattacharjee <127030695+Abhishek-465@users.noreply.github.com>
2024-03-23 19:53:56 +0530

    Update OtherBooks.jsx

--- mern-client/src/home/OtherBooks.jsx
+++ mern-client/src/home/OtherBooks.jsx
@@ -5,7 +5,7 @@ const OtherBooks = () => {
     const[books,setBooks]=useState([]);
 
     useEffect(()=>{
-        fetch("https://localhost:5000/all-books").then(res=>res.json()).then(data => setBooks(data.slice(4,9)))
+        fetch("https://book-store-mqzx.onrender.com/all-books").then(res=>res.json()).then(data => setBooks(data.slice(4,9)))
     },[])
   return (
     <div><BookCards books ={books} headline="Other Books"/></div>
